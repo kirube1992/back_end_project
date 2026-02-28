e71a7f3f95b72f9af0ecaf60b722c02433f6f4c2
kirubel <kirubelaynalem92@gmail.com>
2026-02-28 22:34:20 +0300

    first url api

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello world" });
+});
+
 const PORT = 5001;
 
 app.listen(PORT, () => {
-  console.log(`server running on POrt ${PORT}`);
+  console.log(`server running on Port ${PORT}`);
 });
